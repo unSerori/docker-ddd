0810ddc3d28990f9d06fe18e87c7df8744c8f610
unSerori <pd.ai.374005475@gmail.com>
2024-09-01 16:23:45 +0900

    [add]deploy用のスクリプトでも再ビルド以外のスクリプトを作成

--- deploy-rebuild.sh
+++ deploy-rebuild.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# 削除
-docker compose down --rmi all --remove-orphans
+# 最初の起動や更新の反映に使う
+# 既存のイメージやキャッシュを破棄してビルド&起動し直す
 
-# ビルド
-DOCKER_BUILDKIT=1 docker compose build --no-cache
+# 既存のコンテナの停止とそのイメージの削除 --remove-orphans: Compose ファイルで定義していないサービス用のコンテナも削除
+docker compose down --rmi all --remove-orphans --timeout 15
+
+# キャッシュなしでビルド
+DOCKER_BUILDKIT=1 docker compose -f compose.yml -f compose.deploy.yml build --no-cache
 
 # 起動
-docker compose -f compose.yml -f compose.deploy.yml up -d
+DOCKER_BUILDKIT=1 docker compose -f compose.yml -f compose.deploy.yml up -d
